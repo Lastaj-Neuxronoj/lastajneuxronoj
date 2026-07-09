5bc253c756b0c8637de23ed91c37e42c386d5731
lastajneuxronoj <lastajneuxronoj@gmail.com>
2026-07-09 15:47:35 -0400

    Add cover and redesign post cards
    Integrate twemoji for emoji rendering in cover posts and cards. Redesign post card layout with flexbox to support cover images and emojis.

--- js/content.js
+++ js/content.js
@@ -1,14 +1,20 @@
+function renderEmojiImg(emoji) {
+	if (!emoji) return "";
+
+	return twemoji.parse(emoji, {
+		folder: "svg",
+		ext: ".svg",
+	});
+}
+
 function loadPosts() {
 
 	const postList =
 		document.getElementById("post-list");
 
-
 	if (!postList) return;
 
-
 	postList.innerHTML = "";
-
 
 	fetch("/posts.json")
 	.then(res => res.json())
@@ -16,49 +22,88 @@ function loadPosts() {
 
 		const lang = getCurrentLang();
 
-
 		posts
 		.filter(post => post.file?.[lang])
 		.forEach(post => {
 
-
 			const card =
 				document.createElement("div");
 
-
 			card.className = "post-card";
 
+			let media = "";
+
+			// Imagen (con o sin emoji)
+			if (post.cover) {
+
+				media = `
+					<div class="post-card-media">
+
+						<img
+							class="post-card-cover"
+							src="${post.cover}"
+							alt=""
+						>
+
+						${post.emoji ? `
+							<div class="post-card-emoji">
+								${renderEmojiImg(post.emoji)}
+							</div>
+						` : ""}
+
+					</div>
+				`;
+
+			}
+
+			// Sólo emoji
+			else if (post.emoji) {
+
+				media = `
+					<div class="post-card-emoji-only">
+						${renderEmojiImg(post.emoji)}
+					</div>
+				`;
+
+			}
 
 			card.innerHTML = `
-				<div class="post-number">
-					${post.number || "000"}
+
+				<div class="post-main">
+
+					<div class="post-number">
+						${post.number || "000"}
+					</div>
+
+					<div class="post-content">
+
+						<div class="post-date">
+							${post.date || ""}
+						</div>
+
+						<div class="post-title-index">
+							${post.title[lang]}
+						</div>
+
+						<p class="post-excerpt">
+							${post.excerpt?.[lang] || ""}
+						</p>
+
+					</div>
+
 				</div>
 
-				<div class="post-content">
+				${media}
 
-					<div class="post-date">
-						${post.date || ""}
-					</div>
-
-					<div class="post-title">
-						${post.title[lang]}
-					</div>
-
-					<div class="post-excerpt">
-						${post.excerpt?.[lang] || ""}
-					</div>
-
-				</div>
 			`;
-
 
 			card.onclick = () => {
 				location.href =
 					`/blog/${post.number}-${lang}.html`;
 			};
 
-
 			postList.appendChild(card);
+
 		});
 	});
 }
